0e7ec011bcb91934a8ad9a221efdf45cb75196a6
newton <chiakajunior@yahoo.com>
2021-07-07 15:08:06 +0100

    completed the login and signup

--- Controllers/UserAuth.js
+++ Controllers/UserAuth.js
@@ -40,7 +40,8 @@ exports.checkEmail = async (req, res) => {
         return res.status(500)
             .json({
                 statusCode: 500,
-                message: "server error, check network connection"
+                message: "server error, check network connection",
+                payload: err
             })
     }
 }
@@ -54,8 +55,16 @@ exports.sigunUpUsers = (req, res) => {
     const {email, password, planType, card} = req.body;
     const {firstName, lastName, cardNumber, expirationDate, cvv} = card;
 
+    if(!email||!password||!planType||!firstName||
+        !lastName||cardNumber||expirationDate||!cvv)
+        return res.status(400)
+        .json({
+            statusCode: 400,
+            message: "please fill all fields"
+        })
+
     if(!emailRegex.test(email)){
-        errorArray.push('please fill all field')
+        errorArray.push('invalid email')
     }
     if(password.length < 7) {
         errorArray.push('password should be 7 or more charaters')
@@ -74,7 +83,7 @@ exports.sigunUpUsers = (req, res) => {
         return res.status(400)
             .json({
                 statusCode: 400,
-                message: errorArray
+                message: [...errorArray]
             })
     }
 
@@ -133,4 +142,86 @@ exports.sigunUpUsers = (req, res) => {
         message: 'server error',
         payload: err
     }))
+}
+
+//Login users
+exports.loginUser = (req, res) => {
+    const {email, password} = req.body;
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/gi;
+    const errorArray = [];
+
+    if(!email || !password)
+     return res.status(400)
+        .json({
+            statusCode: 400,
+            message: "please fill all fiels"
+        })
+
+    if(!emailRegex.test(email)){
+        errorArray.push('invalid email address')
+    }
+
+    if(password.length < 7){
+        errorArray.push('password should be 7 or more characters')
+    }
+
+    if(errorArray.length > 0)
+     return res.status(400)
+        .json({
+            statusCode: 400,
+            message: [...errorArray]
+        })
+
+    //check if the email exist in the database
+     userDetails.findOne({email})
+     .then(
+         (user) => {
+            if(!user)
+                return res.status(400)
+                    .json({
+                        statusCode: 400,
+                        message: "incorrect email or password"
+                    })
+            
+            bcyrpt.compare(password, user.pasword)
+            .then(
+                (valid) => {
+                    if(!valid)
+                      return res.status(400)
+                        .json({
+                            statusCode: 400,
+                            message: "incorrect email or password"
+                        })
+
+                    //else, create token and login user.
+                    const token = jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN-SECRET_NUMBER',
+                        { expiresIn: '24h' }
+                      );
+
+                      res.status(201).json({
+                          statusCode: 201,
+                          message: "login successfull",
+                          user
+                      })
+                }
+            ).catch((err) => res.status(500)
+            .json({
+                statusCode: 500,
+                message: "server error",
+                payload: err
+            })
+     )
+
+         }
+
+     ).catch((err) => res.status(500)
+            .json({
+                statusCode: 500,
+                message: "server error",
+                payload: err
+            })
+     )
+    
 }
